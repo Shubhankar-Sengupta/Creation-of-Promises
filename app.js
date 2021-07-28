b88c8d09e7b46c13494b7ab10ab61a95385881e5
Shubhankar <shubhosen98@gmail.com>
2021-07-28 16:30:42 +0530

    Added some CSS styles using styles element and delayedColorChange in JS file

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 
 const fakeServerRequest = (url) => {
     return new Promise((resolve, reject) => { // it returns a Promise object each time the function is executed. It takes a callback which takes two parametrs by default
-    //first is resolve that if the promise is resolved or // reject-the second one if the promise is rejected.
-    // both params expect a callback as well and only one thing can happen at a time.
-        const delay = Math.floor(Math.random()*4500) +500;
+        //first is resolve that if the promise is resolved or // reject-the second one if the promise is rejected.
+        // both params expect a callback as well and only one thing can happen at a time.
+        const delay = Math.floor(Math.random() * 4500) + 500;
         setTimeout(() => {
             resolve(`Data for  ${url}`);
+        }, delay)
+    })  //resolve and reject are two functions either of which on any point of time gets executed inside the Promise() callback.
+
+}
+
+fakeServerRequest('services.com/api/page1')
+    .then((data) => {
+        console.log(data)
+    })
+
+
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => { //returns a new promise object which has methods like then() if resolved else catch() if rejected.
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
         }, delay)
     })
 
 }
 
-fakeServerRequest('services.com/api/page1')
-.then((data)=> {
-    console.log(data)
-
-})
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('green', 1000)) // arrow functions within are returning this constantly.
+    .then(() => delayedColorChange('yellow', 1000))
+    .then(() => delayedColorChange('violet', 1000))
+    .then(() => delayedColorChange('orange', 1000))
